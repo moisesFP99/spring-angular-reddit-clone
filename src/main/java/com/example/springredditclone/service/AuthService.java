1febc69fe419aa177330b48262b0ecba53f75af4
Moises <moisesxd8@gmail.com>
2022-03-23 00:06:38 -0300

    email service

--- src/main/java/com/example/springredditclone/service/AuthService.java
+++ src/main/java/com/example/springredditclone/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.springredditclone.service;
 
 import java.time.Instant;
+import java.util.UUID;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -8,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.model.User;
+import com.example.springredditclone.model.VerificationToken;
 import com.example.springredditclone.repository.UserRepository;
+import com.example.springredditclone.repository.VerificationTokenRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -18,6 +21,7 @@ public class AuthService {
 
 	private final PasswordEncoder passwordEncoder;	
 	private final UserRepository userRepository;
+	private final VerificationTokenRepository verificationTokenRepository;
 	
 	@Transactional
 	public void signup(RegisterRequest registerRequest) {
@@ -29,7 +33,18 @@ public class AuthService {
 		user.setEnabled(false);
 		
 		userRepository.save(user);
+		
+		String token = generateVerificationToken(user);
+	}
+		
+		private String generateVerificationToken(User user) {
+			String token = UUID.randomUUID().toString();
+			VerificationToken verificationToken = new VerificationToken();
+			verificationToken.setToken(token);
+			verificationToken.setUser(user);
+			
+			verificationTokenRepository.save(verificationToken);
+			return token;
+		}
 
 	}
-
-}
